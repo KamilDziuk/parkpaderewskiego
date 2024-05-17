025010dc4f42c2fcce7acd0fae42ccce8c17df68
kamildziuk <kontakt@kamildziuk.pl>
2024-05-17 13:37:16 +0200

    Adding a mobile version (css) v3

--- above-ground-parking-lot-A.php
+++ above-ground-parking-lot-A.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 
-<link rel="stylesheet" type="text/css" href="css/undergroundA.css">
+<link rel="stylesheet" type="text/css" href="css/above-ground-parking-lot.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <script src="https://smtpjs.com/v3/smtp.js"></script>
 <script src="js/script.js" defer></script>
