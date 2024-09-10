731c1e790718ebd6ce7ff9265603bc74afaad212
kamildziuk <kontakt@kamildziuk.pl>
2024-09-10 12:39:47 +0200

    1: Adding a media tab. 2: Adding a page (media) 3: On the media page, adding a forbes button and adding iframe tags from the video path. 4: Adding media.html, gallery.css and media.js files.

---
+++ js/media.js
@@ -0,0 +1,27 @@
+// navigation bar start
+document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".hamburger").classList.toggle("is-active");
+    document.querySelector(".navbar__menu").classList.toggle("navbar__menu--active");
+    document.querySelector(".top").classList.toggle("top--open");
+    });
+    window.addEventListener("scroll",  () =>{
+    let scroll = window.pageYOffset || document.documentElement.scrollTop;
+    if (scroll >= 60) {
+    document.querySelector(".top").classList.add("top--scrolling");
+    } else {
+    document.querySelector(".top").classList.remove("top--scrolling");
+    }
+    });
+    // navigation bar end
+    
+    // popup start
+    let showPopupButton = document.getElementById('showPopup');
+    let popup = document.getElementById('popup');
+    let closePopupButton = document.getElementById('closePopup');
+    showPopupButton.addEventListener('click', () => {
+    popup.style.display = 'block';
+    });
+    closePopupButton.addEventListener('click', () => {
+    popup.style.display = 'none';
+    });
+    // popup end
