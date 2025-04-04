d6363b27f7dcfb5c090fbdbb91b5a8d7290a9252
kamildziuk <kamildziukwebdeveloper@gmail.com>
2025-04-04 21:49:14 +0200

    feature: added footer function and exported
    added footer function and exported its footer.js file

--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -1,4 +1,8 @@
+
+export  function footer() {
 fetch("footer.html")
 .then(resposne => resposne.text())
 .then(footer => document.querySelector('#footer-container').innerHTML = footer);
+};
 
+footer();
